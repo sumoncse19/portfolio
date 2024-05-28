0c01689d8d5c5cdc365a3853d8634dddd5d7c201
sumoncse19 <mdsumoncse19@gmail.com>
2024-05-29 00:30:37 +0600

    updated portfolio

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
   {
     id: 1,
-    title: "Software Engineer I",
-    company: "Developer eXperience Hub",
-    duration: "(DEC 2021 - Present)",
+    title: "Front End Developer",
+    company: "BusyQA",
+    duration: "(MAY 2024 - Present)",
   },
   {
     id: 2,
+    title: "Software Engineer I",
+    company: "Developer eXperience Hub",
+    duration: "(DEC 2021 - MAY 2024)",
+  },
+  {
+    id: 3,
     title: "Frontend Developer",
     company: "Hi-Tech Bangla",
     duration: "(JULY 2021 - NOV 2021)",
