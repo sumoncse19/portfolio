b99a2e6c78f8577d2c448fc1b06df2fba7e920c6
sumoncse19 <mdsumoncse19@gmail.com>
2024-06-04 13:17:28 +0600

    update resume

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: "Front End Developer",
-    company: "BusyQA",
-    duration: "(MAY 2024 - Present)",
+    title: "Software Engineer I",
+    company: "Developer eXperience Hub",
+    duration: "(DEC 2021 - Present)",
   },
   {
     id: 2,
-    title: "Software Engineer I",
-    company: "Developer eXperience Hub",
-    duration: "(DEC 2021 - MAY 2024)",
-  },
-  {
-    id: 3,
     title: "Frontend Developer",
     company: "Hi-Tech Bangla",
-    duration: "(JULY 2021 - NOV 2021)",
+    duration: "(JUNE 2021 - NOV 2021)",
   },
 ];
